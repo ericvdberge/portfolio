09e62cffdcd63ff8cb77d19ceb2f714ce54e0538
Eric van den Berge <e.v.d.berge@infoland.nl>
2024-01-10 22:41:55 +0100

    Add Tailwind CSS and update app layout

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col p-16 justify-center items-center">
+    <img src="/img/profile.jpg" class="mx-auto w-[200px]" />
+    <p class="text-2xl">Eric van den Berge</p>
   </div>
 </template>
