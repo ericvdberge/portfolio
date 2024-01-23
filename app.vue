fbda2be07d3d09b45e4fca39b1a454c36ff187db
ericvdberge <32539664+ericvdberge@users.noreply.github.com>
2024-01-23 16:38:50 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,6 +2,6 @@
   <div class="flex flex-col p-16 justify-center items-center">
     <img src="/img/profile.jpg" class="mx-auto w-[200px]" />
     <p class="text-2xl">Eric van den Berge</p>
-    <p class="text-md">Full stack developer</p>
+    <p class="text-md">Full stack developer innovation</p>
   </div>
 </template>
